1c5f3f60e8561d56d8bd460a72f8ef8383d182e1
Matthew Edwards <matt.e.edwards@gmail.com>
2016-06-02 18:37:50 +1000

    Adds dabble Homebrew formula
    * Adds support for installing release 0.1.0
    * Adds support for installing HEAD

---
+++ HomebrewFormula/dabble.rb
@@ -0,0 +1,28 @@
+class Dabble < Formula
+  desc "The simplest way to load a Scala library into the REPL"
+  homepage "https://github.com/ssanj/dabble"
+  url "https://github.com/ssanj/dabble/releases/download/v0.1.0/dabble", :using => :nounzip
+  version "0.1.0"
+  sha256 "554cd5ffd5d198dddf23bbd8d39eaba9d801fa2c427ab765c4bd26669c52f9d4"
+  head "https://github.com/ssanj/dabble.git"
+
+  if build.head?
+    depends_on :java => "1.7+"
+    depends_on "sbt" => :build
+  end
+
+  def install
+    if build.head?
+      # ENV.java_cache
+      system "sbt", "assembly"
+      bin.install "target/scala-2.11/dabble" => "dabble"
+    else
+      bin.install "dabble" => "dabble"
+    end
+  end
+
+  test do
+    expected_output = "Dabble version:"
+    assert_includes shell_output("#{bin}/dabble -version"), expected_output
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# dabble-tap
-The simplest way to load a Scala library into the REPL
+# Dabble Homebrew Tap
+This a [brew](https://github.com/mxcl/homebrew) tap containing the formula for installing [dabble](https://github.com/ssanj/dabble) using brew.
+
+## Usage
+
+First, add this tap to your Brew.
+
+    brew tap edwardsmatt/dabble-tap
+
+Now you will be able to install the latest dabble release:
+
+    brew install dabble
+
+or the latest HEAD using:
+
+    brew install dabble --HEAD
+
+or, if you want more information about the install you can use:
+
+    brew install dabble --HEAD --debug --verbose
